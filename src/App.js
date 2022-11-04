f75721ddc61ab778ba94b6197a5969939ca331b5
Joel Watkins <eck087@yahoo.co.uk>
2022-11-04 11:49:33 +0000

    countdown implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [fontSize, setFontSize] = useState(2);
+  const [isDecreaseActive, setIsDecreaseActive] = useState(false);
+  const [isIncreaseActive, setIsIncreaseActive] = useState(false);
+
+  function decreaseCount() {
+    if (count < 1) {
+      return;
+    }
+    if (count < 2) {
+      setIsDecreaseActive(true);
+    }
+    setIsIncreaseActive(false);
+    setCount(--count);
+    setFontSize(fontSize - 0.2);
+  }
+
+  function increaseCount() {
+    if (count > 9) {
+      return;
+    }
+    if (count > 8) {
+      setIsIncreaseActive(true);
+    }
+    setIsDecreaseActive(false);
+    setCount(++count);
+    setFontSize(fontSize + 0.2);
+  }
+
+  function halveCount() {
+    setCount(Math.floor(count / 2));
+  }
+
+  function doubleCount() {
+    setCount(count * 2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter App</h1>
+      <p style={{ fontSize: fontSize + "rem" }}>{count}</p>
+      <button
+        onClick={decreaseCount}
+        className={isDecreaseActive ? "move-down" : ""}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={increaseCount}
+        className={isIncreaseActive ? "move-down" : ""}
+      >
+        Increment
+      </button>
+      <button onClick={halveCount}>Halve</button>
+      <button onClick={doubleCount}>Double</button>
+    </>
   );
 }
 
